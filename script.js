d37f7d23d57a7ec284d04f59661bcefb416c1797
dalto135 <67397294+dalto135@users.noreply.github.com>
2021-03-04 20:38:41 -0500

    js tinkering

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ function yelpApi() {
     let requestUrl = "https://api.yelp.com/v3/autocomplete?text=del&latitude=37.786882&longitude=-122.399972";
     console.log(requestUrl);
 
+    let access = {Access-Control-Allow-Origin: "*"};
+    let header = {Authorization: "Bearer O_IPrWWCmaXkkcUPJb0EjA:0TPfYzbBQDZOLb9MddQtWCiD1E7FLunXqWtcrc6Pwc2_aOxyJ9eW1yDiIcuIp_83vF0kprF5P0pVtDYpp4gU0S4r3oyxXFdKdbLUtPmW2X4Qfc8Kx9fQgeLjkfI-YHYx"};
+
     fetch(requestUrl)
         .then(function(response) {
             return response.json();
@@ -17,6 +20,23 @@ function yelpApi() {
             console.log("Error");
         });
 }
+
+
+function cheater() {
+    var data = null;
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://api.yelp.com/v3/businesses/search?text=coffee&latitude=37.786882&longitude=-122.399972");
+    xhr.setRequestHeader("Authorization", "Bearer O_IPrWWCmaXkkcUPJb0EjA:0TPfYzbBQDZOLb9MddQtWCiD1E7FLunXqWtcrc6Pwc2_aOxyJ9eW1yDiIcuIp_83vF0kprF5P0pVtDYpp4gU0S4r3oyxXFdKdbLUtPmW2X4Qfc8Kx9fQgeLjkfI-YHYx");
+
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            data = JSON.parse(this.responseText);
+            console.log(data);
+        }
+    });
+    xhr.send(data);
+}
+
 
 function recipeApi() {
     let requestUrl = "";
