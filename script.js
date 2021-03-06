328394197cb1e24cfaf0ecedf0ac605aa20830dc
dalto135 <67397294+dalto135@users.noreply.github.com>
2021-03-06 13:18:40 -0500

    js

--- script.js
+++ script.js
@@ -2,16 +2,14 @@
 let input = document.querySelector("input");
 
 //Event listener
-input.addEventListener("click", recipeApi);
+// input.addEventListener("click", recipeApi);
+// input.addEventListener("click", yelpApi);
 
 //Yelp API function
 function yelpApi() {
     let requestUrl = "https://api.yelp.com/v3/autocomplete?text=del&latitude=37.786882&longitude=-122.399972&appid=0TPfYzbBQDZOLb9MddQtWCiD1E7FLunXqWtcrc6Pwc2_aOxyJ9eW1yDiIcuIp_83vF0kprF5P0pVtDYpp4gU0S4r3oyxXFdKdbLUtPmW2X4Qfc8Kx9fQgeLjkfI-YHYx";
     console.log(requestUrl);
 
-    // let access = {Access-Control-Allow-Origin: "*"};
-    // let header = {Authorization: "Bearer O_IPrWWCmaXkkcUPJb0EjA:0TPfYzbBQDZOLb9MddQtWCiD1E7FLunXqWtcrc6Pwc2_aOxyJ9eW1yDiIcuIp_83vF0kprF5P0pVtDYpp4gU0S4r3oyxXFdKdbLUtPmW2X4Qfc8Kx9fQgeLjkfI-YHYx"};
-
     fetch(requestUrl)
         .then(function(response) {
             return response.json();
@@ -24,51 +22,35 @@ function yelpApi() {
             console.log("Error");
         });
 }
-
-
-// function cheater() {
-//     var data = null;
-//     var xhr = new XMLHttpRequest();
-//     xhr.open("GET", "https://api.yelp.com/v3/businesses/search?text=coffee&latitude=37.786882&longitude=-122.399972");
-//     xhr.setRequestHeader("Authorization", "Bearer O_IPrWWCmaXkkcUPJb0EjA:0TPfYzbBQDZOLb9MddQtWCiD1E7FLunXqWtcrc6Pwc2_aOxyJ9eW1yDiIcuIp_83vF0kprF5P0pVtDYpp4gU0S4r3oyxXFdKdbLUtPmW2X4Qfc8Kx9fQgeLjkfI-YHYx");
-
-//     xhr.addEventListener("readystatechange", function () {
-//         if (this.readyState === 4) {
-//             data = JSON.parse(this.responseText);
-//             console.log(data);
-//         }
-//     });
-//     xhr.send(data);
-// }
 
 //Recipe API function
 function recipeApi() {
-    let requestUrl = "";
+    let requestUrl = "https://api.spoonacular.com/recipes/716429/information?apiKey=349863eb6f0f4135b4d518b60c73d656&includeNutrition=true";
     console.log(requestUrl);
 
     fetch(requestUrl)
-        .then(function(response) {
+        .then(response => {
             return response.json();
         })
-        .then(function(data) {
+        .then(data => {
             console.log(data);
-            //Type code here
-        })
-        .catch(function() {
-            console.log("Error");
-        });
+        }) 
+        .catch(err => {
+            console.error(err);
+    });
 }
 
-fetch("https://api.spoonacular.com/recipes/716429/information?apiKey=349863eb6f0f4135b4d518b60c73d656&includeNutrition=true"
- 
-	
-)
-.then(response => {
-	return response.json();
-})
-.then(data => {
-    console.log(data);
-}) 
-.catch(err => {
-	console.error(err);
-});
+//Test
+let requestUrl = "https://api.spoonacular.com/recipes/716429/information?apiKey=349863eb6f0f4135b4d518b60c73d656&includeNutrition=true";
+    console.log(requestUrl);
+
+    fetch(requestUrl)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+        }) 
+        .catch(err => {
+            console.error(err);
+    });
