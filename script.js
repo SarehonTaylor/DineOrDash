edf87a53a9105c5b743cb863cd555359512825d4
dalto135 <67397294+dalto135@users.noreply.github.com>
2021-03-06 22:45:03 -0500

    documenu access is working, making progress towards spoonacular

--- script.js
+++ script.js
@@ -1,30 +1,64 @@
-//Global variables
-let input = document.querySelector("input");
-
-//Event listener
-// input.addEventListener("click", recipeApi);
-// input.addEventListener("click", yelpApi);
-
-//Yelp API function
-function yelpApi() {
-    let requestUrl = "https://api.yelp.com/v3/autocomplete?text=del&latitude=37.786882&longitude=-122.399972&appid=0TPfYzbBQDZOLb9MddQtWCiD1E7FLunXqWtcrc6Pwc2_aOxyJ9eW1yDiIcuIp_83vF0kprF5P0pVtDYpp4gU0S4r3oyxXFdKdbLUtPmW2X4Qfc8Kx9fQgeLjkfI-YHYx";
-    console.log(requestUrl);
+let city = "columbus";
+let food = "pizza";
+// getCoords(city);
+recipeApi(food);
+//Takes a city string as an argument and inputs the latitude and longitude of that city as arguments for
+//the restApi() function
+function getCoords(city) {
+    let requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=de53a40654766cb8ce20288a99c9f736";
 
     fetch(requestUrl)
-        .then(function(response) {
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        console.log("Openweather data:");
+        console.log(data);
+
+        let lat = data["coord"]["lat"];
+        let lon = data["coord"]["lon"];
+        restApi(lat, lon, food);
+      })
+      .catch(function() {
+        console.log("Error");
+      });
+}
+
+//Takes the coordinates of the inputed city and the requested food item and prints 10 results to the console
+function restApi(lat, lon, food) {
+    let requestUrl = "https://documenu.p.rapidapi.com/menuitems/search/geo?lat=" + lat + "&lon=" + lon + "&distance=5&size=10&page=1&search=" + food;
+    console.log(requestUrl);
+    
+    fetch(requestUrl, {
+	"method": "GET",
+	"headers": {
+		"x-api-key": "3961f418c97acd355c657af27c9e007c",
+		"x-rapidapi-key": "f8fb3fa959msh5a99371fad537f4p136473jsn05a9c31b8f9d",
+		"x-rapidapi-host": "documenu.p.rapidapi.com"
+	}
+    })
+        .then(response => {
             return response.json();
         })
-        .then(function(data) {
+        .then(data => {
+            console.log("Documenu data:");
             console.log(data);
-            //Type code here
+
+            for (let i = 0; i < data["data"].length; i++) {
+                let num = i + 1;
+                console.log(num + ".");
+                console.log("Restaurant: " + data["data"][i]["restaurant_name"]);
+                console.log("Food: " + data["data"][i]["menu_item_name"]);
+                console.log("Price: " + data["data"][i]["menu_item_price"]);
+            }
         })
-        .catch(function() {
-            console.log("Error");
+        .catch(err => {
+            console.error(err);
         });
 }
 
-//Recipe API function
-function recipeApi() {
+//Takes the food item as an argument and outputs a list of recipes
+function recipeApi(food) {
     let requestUrl = "https://api.spoonacular.com/recipes/716429/information?apiKey=349863eb6f0f4135b4d518b60c73d656&includeNutrition=true";
     console.log(requestUrl);
 
@@ -33,24 +67,42 @@ function recipeApi() {
             return response.json();
         })
         .then(data => {
+            console.log("Spoonacular data:");
             console.log(data);
+            console.log(food);
+
+            for (let i = 0; i < 10; i++) {
+                let num = i + 1;
+                console.log(num + ". Recipe");
+            }
         }) 
         .catch(err => {
             console.error(err);
     });
 }
 
-//Test
-let requestUrl = "https://api.spoonacular.com/recipes/716429/information?apiKey=349863eb6f0f4135b4d518b60c73d656&includeNutrition=true";
-    console.log(requestUrl);
+// function recipeApi(food) {
+//     fetch("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/queries/analyze?q=pizza", {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-key": "f8fb3fa959msh5a99371fad537f4p136473jsn05a9c31b8f9d",
+// 		"x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+// 	}
+//     })
+//         .then(response => {
+//             return response.json();
+//         })
+//         .then(data => {
+//             console.log("Rapidapi Spoonacular data:");
+//             console.log(data);
+//             console.log(food);
 
-    fetch(requestUrl)
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            console.log(data);
-        }) 
-        .catch(err => {
-            console.error(err);
-    });
+//             for (let i = 0; i < 10; i++) {
+//                 let num = i + 1;
+//                 console.log(num + ". Recipe");
+//             }
+//         })
+//         .catch(err => {
+//             console.error(err);
+//         });
+// }
